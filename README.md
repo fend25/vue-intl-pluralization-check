0f05c7d2a369beef991d7750f972294562cbd8e7
Alex Saft <fend25@gmail.com>
2018-02-20 17:34:56 +0300

    code example

---
+++ README.md
@@ -0,0 +1,15 @@
+# vue-intl-pluralization-check
+
+> A Vue.js project, example for issue with pluralization.
+
+Issue: `$formatMessage` doesn't provide correct pluralization. Numbers which should be mapped as `two`, `few` and `many` are mapped to `other`.
+
+
+## How to run
+
+``` bash
+git clone https://github.com/fend25/vue-intl-pluralization-check.git
+cd vue-intl-pluralization-check
+yarn
+npm start
+```
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="app">
+
+  <p>{{$formatDate(new Date(), { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}}</p>
+  <p>{{$formatNumber(7.5, { currency: 'rub' })}}</p>
+  <p>{{JSON.stringify(list)}}</p>
+  <span v-for="n in list">
+			{{$formatMessage({id: 'things'}, {n})}}<br/>
+		</span>
+</div>
+</template>
+
+<script>
+const list = new Array(30).fill().map((e, i) => i) //array of numbers from 0 to 29
+console.log(list.map(i => [i, typeof i]))
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      list
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+html, body {
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
+  text-align: center;
+  margin-top: 3rem;
+}
+</style>
