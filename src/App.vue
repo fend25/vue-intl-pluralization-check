e3a2fae2f8f59e9d29970c9faca008a061c826a2
Alex Saft <fend25@gmail.com>
2018-02-20 17:41:23 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
 <div id="app">
-
   <p>{{$formatDate(new Date(), { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}}</p>
   <p>{{$formatNumber(7.5, { currency: 'rub' })}}</p>
   <p>{{JSON.stringify(list)}}</p>
   <span v-for="n in list">
-			{{$formatMessage({id: 'things'}, {n})}}<br/>
-		</span>
+    {{$formatMessage({id: 'things'}, {n})}}<br/>
+  </span>
 </div>
 </template>
 
 <script>
 const list = new Array(30).fill().map((e, i) => i) //array of numbers from 0 to 29
-console.log(list.map(i => [i, typeof i]))
+console.log(...list.map(i => [i, typeof i]))
 
 export default {
   name: 'app',
--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import VueIntl from 'vue-intl';
+import VueIntl from 'vue-intl'
 
 Vue.use(VueIntl)
 
 Vue.setLocale('ru')
 
 Vue.registerMessages('ru', {
-	things: `how much: {n, plural,
-		zero {no any}
-    one {one}
-    two {two}
-    few {{n} few}
-    many {{n} many}
-    other {{n} other}
-	}`
+  things: `{n}. how much: {n, plural,
+zero {no any}
+one {one}
+two {two}
+few {{n} few}
+many {{n} many}
+other {{n} other}
+}`
 })
 Vue.registerFormats('ru', {
-    number: {
-        rub: { style: 'currency', currency: 'RUB' }
-    }
+  number: {
+    rub: {style: 'currency', currency: 'RUB'}
+  }
 })
 
 new Vue({
-	el: '#app',
-	render: h => h(App)
+  el: '#app',
+  render: h => h(App)
 })
